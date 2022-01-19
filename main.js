b370eb45575f19f77f891b065e719ce4c71b6f62
Ridhianand <90387640+Ridhianand@users.noreply.github.com>
2022-01-19 17:29:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var name1= "Steve";
+console.log(name1);
+var last_name="Jobs";
+console.log(last_name);
+var full_name= name1.concat(last_name);
+console.log(full_name);
+var capital_word= "Calafornia";
+var lower_caseword= capital_word.toLowerCase();
+console.log(lower_caseword);
+var small_word="Tiger";
+var upper_caseword= small_word.toUpperCase();
+console.log(upper_caseword);
+var maximum_number= Math.max(7282,8273);
+console.log(maximum_number)
+var minimum_number= Math.min(9304,7478);
+console.log(minimum_number)
+var a= 5;
+var b = 10;
+var add= a+b;
+console.log(add)
+var c= 11;
+var d= 7;
+var minus= 11-7;
+console.log(minus)
+var e= 5;
+var f= 3;
+var multiply= e*f;
+console.log(multiply)
+var g= 2;
+var h=4 ;
+var divide= g/h;
+console.log(divide)
